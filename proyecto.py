d84dea40f73f30b84c5ee7d58924618f5ab69bd4
Miguel <miguelangelbolanolopez@gmail.com>
2023-12-01 12:38:08 -0500

    Prim
    Se implemento el algoritmo de Prim

--- proyecto.py
+++ proyecto.py
@@ -1,68 +1,80 @@
 import random
 
-#   Clase que representa un nodo en el grafo.
+# Clase que representa un nodo en el grafo.
 class Nodo:
     def __init__(self, nombre, estado="sano"):
-        #   estado: Estado del nodo. Por defecto es "sano".
         self.estado = estado
         self.nombre = nombre
     
     def __str__(self):
         return f"{self.nombre} ({self.estado})"
         
-#   Clase que repesenta el grafo.
+# Clase que repesenta el grafo.
 class Grafo:
     def __init__(self):
-        #   nodos: Lista de nodos en el grafo.
         self.nodos = set()
         self.aristas = {}
         
-    #   Agrega un nodo al grafo.
     def agregar_nodo(self, nodo):
-        #   nodo: Nodo a agregar al grafo.
         self.nodos.add(nodo)
         self.aristas[nodo] = {}
 
-    #   Agrega una arista entre dos nodos en el grafo.
     def agregar_arista(self, nodo1, nodo2, peso):
         if nodo1 in self.nodos and nodo2 in self.nodos:
             self.aristas[nodo1][nodo2] = peso
             self.aristas[nodo2][nodo1] = peso
 
-    #   Obtiene la lista de nodos en el grafo.
     def obtener_nodos(self):
         return self.nodos
     
-class Main:
-    def __init__(self, num_infectados_iniciales=1, num_pasos_tiempo=5):
-        #   num_infectados_iniciales: Número de nodos infectados inicialmente. Por defecto es 1.
-        #   num_pasos_tiempo: Número de pasos de tiempo a simular. Por defecto es 5.
+    def obtener_aristas(self, nodo):
+        return self.aristas[nodo]
+    
+    def prim(self, nodo_inicial):
+        visitados = set()
+        visitados.add(nodo_inicial)
+        aristas_seleccionadas = []
         
+        while len(visitados) < len(self.nodos):
+            nodo_actual = None
+            arista_minima = None
+            peso_minimo = float('inf')
+            
+            for nodo in visitados:
+                for vecino, peso in self.aristas[nodo].items():
+                    if vecino not in visitados and peso < peso_minimo:
+                        nodo_actual = nodo
+                        arista_minima = (nodo, vecino)
+                        peso_minimo = peso
+            
+            if arista_minima:
+                aristas_seleccionadas.append(arista_minima)
+                visitados.add(arista_minima[1])
+        
+        return aristas_seleccionadas
+    
+class Main:
+    def __init__(self):
         self.grafo = Grafo()
-        # Creando nodos
         nodo_a = Nodo("A", "infectado")
         nodo_b = Nodo("B")
         nodo_c = Nodo("C")
         nodo_d = Nodo("D")
         nodo_e = Nodo("E")
-        # Agregando Nodos
         self.grafo.agregar_nodo(nodo_a)
         self.grafo.agregar_nodo(nodo_b)
         self.grafo.agregar_nodo(nodo_c)
         self.grafo.agregar_nodo(nodo_d)
         self.grafo.agregar_nodo(nodo_e)
-        # Agregando Aristas
         self.grafo.agregar_arista(nodo_a, nodo_b, 5)
         self.grafo.agregar_arista(nodo_a, nodo_c, 3)
         self.grafo.agregar_arista(nodo_b, nodo_d, 7)
         self.grafo.agregar_arista(nodo_c, nodo_d, 2)
         self.grafo.agregar_arista(nodo_c, nodo_e, 4)
         self.grafo.agregar_arista(nodo_d, nodo_e, 1)
+        "self.pintar_grafo()"
+        self.prim(nodo_a)
     
-        self.pintar_grafo()
-        "self.simular_propagacion(num_pasos_tiempo, num_infectados_iniciales)"
-    
-    # Pinta el grafo original 
     def pintar_grafo(self):
         for nodo in self.grafo.obtener_nodos():
             print(f"Nodo {nodo}:")
@@ -70,16 +82,14 @@ class Main:
                 print(f"  -> Nodo {vecino}, Peso: {peso}")
             print()
     
-    #   Simula la propagación de la enfermedad en el grafo.
-    def simular_propagacion(self, num_pasos_tiempo, num_infectados_iniciales):
-        poblacion = self.grafo.obtener_nodos()
+    def prim(self, nodo_inicial):
+        aristas_seleccionadas = self.grafo.prim(nodo_inicial)
+        print("Aristas seleccionadas:")
+        for arista in aristas_seleccionadas:
+            print(f"{arista[0]} - {arista[1]} : {self.grafo.aristas[arista[0]][arista[1]]}")
+    
         
-        
-        #   num_pasos_tiempo: Número de pasos de tiempo a simular.
-        for i in range(num_pasos_tiempo):
-            print(f"Paso de tiempo {i}")
-            self.grafo.prim()
 
-#   Ejecuta el simulador.
+
 if __name__ == "__main__":
     simulador = Main()
